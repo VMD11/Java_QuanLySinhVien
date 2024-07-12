94ac6b9c685e595363e41bd45759e38c7fe0b528
manhdung02 <vmd111202@gmail.com>
2024-07-13 00:51:37 +0700

    create Model

---
+++ Java_QuanLySinhVien/src/Model/Khoa.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 84362
+ */
+public class Khoa {
+    public int maKhoa;
+    public String tenKhoa;
+
+    public Khoa() {
+    }
+
+    public int getMaKhoa() {
+        return maKhoa;
+    }
+
+    public void setMaKhoa(int MaKhoa) {
+        this.maKhoa = MaKhoa;
+    }
+
+    public String getTenKhoa() {
+        return tenKhoa;
+    }
+
+    public void setTenKhoa(String TenKhoa) {
+        this.tenKhoa = TenKhoa;
+    }
+    
+    
+}
