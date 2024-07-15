ab487b3c442cf8ac493305425fb690c1e94e7a96
manhdung02 <vmd111202@gmail.com>
2024-07-16 03:00:19 +0700

    add class User

---
+++ Java_QuanLySinhVien/src/Model/User.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 84362
+ */
+public class User {
+    public String userName;
+    public String password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userName=" + userName + ", password=" + password + '}';
+    }
+    
+}
